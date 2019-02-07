8e22113351374e407b77751ad1d76a3b814a0336
Mario Menezes Costa <mariomenezescosta@gmail.com>
2019-02-07 03:55:11 -0300

    litte changes

--- guide.sh
+++ guide.sh
@@ -1,8 +1,14 @@
 # TODO
-need to change de database address from localhost to db(name that a gave to postgres instance)
+need to change de database address from localhost to db(name that a gave to postgres instance) in settings.py.dev, inside casepro
 
 # TODO
 need to split config files in folders, like db folder, django folder, jenkis folder
 
 # TODO
 Change ports to expose in db config, like reference https://stackoverflow.com/questions/40801772/what-is-the-difference-between-docker-compose-ports-vs-expose
+
+sudo docker-compose down
+
+sudo docker-compose build --no-cache
+
+sudo docker-compose up
